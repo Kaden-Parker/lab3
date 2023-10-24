6af3503585c49685a05e593392ab1a64d2b1cf7b
Kaden-Parker <130111586+Kaden-Parker@users.noreply.github.com>
2023-10-24 13:13:58 -0700

    Update ArrayExamples.java

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,37 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReversed2() {
+    int[] input1 = {1, 2, 3};
+    assertArrayEquals(new int[]{3, 2, 1}, ArrayExamples.reversed(input1));
+  }
+  @Test 
+	public void testReverseInPlace2() {
+    int[] input1 = {1,2,3,4};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{4,3,3,4}, input1);
+	}
+  @Test 
+	public void averageWithoutLowestEmp() {
+    double[] arr = {};
+    ArrayExamples.averageWithoutLowest(arr);
+    assertEquals(0.0, ArrayExamples.averageWithoutLowest(arr), 0.01);
+
+  }
+
+  @Test 
+	public void averageWithoutLowestOne() {
+    double[] arr = {5};
+    ArrayExamples.averageWithoutLowest(arr);
+    assertEquals(0.0, ArrayExamples.averageWithoutLowest(arr), 0.01);
+  }
+  
+  @Test 
+	public void averageWithoutLowestA() {
+    double[] arr = {1, 2, 3};
+    ArrayExamples.averageWithoutLowest(arr);
+    assertEquals(0.0, ArrayExamples.averageWithoutLowest(arr), 0.01);
+  }
 }
